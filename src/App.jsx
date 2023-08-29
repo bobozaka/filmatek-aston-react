a987713b0e724f0e872f80aeaccb10971d8e0f63
bobozaka <86849544+bobozaka@users.noreply.github.com>
2023-08-29 15:33:16 +0300

    feature authorization via firebase

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,31 @@ import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss'; // Импорт стилей
 import Navbar from './componnets/Navbar/Navbar';
 import Home from './pages/Home';
+import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login/Login';
+import Account from './pages/Account';
+import Signup from './pages/Signup/Signup';
+import ProtectedRoute from './componnets/ProtectedRoute';
 
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- src/componnets/Movie/Movie.jsx
+++ src/componnets/Movie/Movie.jsx
@@ -7,7 +7,11 @@ function Movie({ item }) {
   const [like] = useState(false);
   return (
     <div className={styles.movie__card} key={item.id}>
-      <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
+      {item.backdrop_path ? (
+        <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
+      ) : (
+        <div>No Image Available</div>
+      )}
       <div>
         <p className={styles.movie__card_title}>{item.title}</p>
         <p className={styles.movie__card_like}>{like ? <FaHeart /> : <FaRegHeart />}</p>
@@ -19,7 +23,7 @@ Movie.propTypes = {
   item: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    backdrop_path: PropTypes.string.isRequired,
+    backdrop_path: PropTypes.string,
   }).isRequired,
 };
 
